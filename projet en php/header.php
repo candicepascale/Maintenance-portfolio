03ed048757c93ae299765a45736a4404424f0ab8
candicepascale <candice2ng@gmail.com>
2026-02-13 00:41:48 -0500

    Code du header et du footer #1

---
+++ projet en php/header.php
@@ -0,0 +1,71 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/logo.ico" type="image/x-icon">
+    <script src="js/agrandissement.js" defer></script>
+    <link rel="stylesheet" href="style/style.css">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+      integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer">
+
+    <script src="js/decompte.js" defer></script>
+    <title>jewelrybypc</title>
+    <script src="js/caroussel.js" defer></script>
+    <script src="js/compte.js" defer></script>
+    <script src="js/validation.js" defer></script>
+    <script src="js/defautText.js" defer></script>
+    <script src="js/acordeon.js" defer></script>
+    <script src="js/lightbox-min.js" defer></script>
+     <script src="js/bijou.js" defer></script>
+
+</head>
+
+<body>
+    <h1>JEWELRY BY PC</h1>
+
+    <!--menu de navigation-->
+    <!-- nav>ul>li*4>a -->
+    <!-- ajout partie imbriquée -->
+    <!-- li>a+ul>li*2>a -->
+<header>
+    <nav class="navbar">
+        <span class="navbar-toggle" id="js-navbar-toggle">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+        </span>
+        <ul class = "menu" id="js-menu">
+            <li>
+                <a href="index.php">
+                    <i class="fa-solid fa-house-chimney"></i>Accueil
+                </a>
+            </li>
+            <li>
+                <a href="categorie.php">
+                    <i class="fa-solid fa-shapes"></i>Catégories</a>
+                <ul>
+                    <li><a href="femme.php"><i class="fa-solid fa-person-dress"></i>Femmes</a></li>
+                    <li><a href="homme.php"><i class="fa-solid fa-person"></i>Hommes</a></li>
+                    <li><a href="enfant.php"><i class="fa-solid fa-children"></i>Enfants</a></li>
+                </ul>
+            </li>
+
+
+            <li><a href="mission.php"><i class="fa-solid fa-pen"></i>Mission</a></li>
+            <li><a href="espace-membre.php"><i class="fa-solid fa-user"></i>Espace Membre</a></li>
+            <li><a href="contact.php"><i class="fa-solid fa-address-book"></i>Contact</a></li>
+            <li><a href="faq.php"><i class="fa-solid fa-exclamation"></i>FAQ</a></li>
+        </ul>
+    </nav>
+</header>
+     <script src="js/script.js"  defer></script>
+    <a href="images/headers.jpg"  target="_blank" rel="noopener noreferrer"><img src="images/headers.jpg" alt="header" class="header" title="une image de bijoux"/></a>
